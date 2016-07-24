5c1f0938f6127c484da75af87347626d95a37b4f
Michael Prichard <michael.prichard@willowtreeapps.com>
2016-07-24 13:56:41 -0400

    Update wombat!

--- bot.clj
+++ bot.clj
@@ -2,9 +2,6 @@
 
 (defn run
   [{:keys  [arena state bot_id energy spawn-bot?] :as step-details}]
-
-  (println "michael")
-
   (let [command-options [[{:cmd  "SHOOT"
                            :metadata  {:direction (rand-nth  [0 1 2 3 4 5 6 7]) :energy 20}}
                           {:cmd  "SET_STATE"
